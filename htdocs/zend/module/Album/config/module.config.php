9835ceeb6da0525ba1fcbda2402726eaaeb11bca
Sebastião Junio Menezes Campos <juniobc@gmail.com>
2014-09-27 14:03:29 -0300

    Criando o modulo Album

--- htdocs/zend/module/Album/config/module.config.php
+++ htdocs/zend/module/Album/config/module.config.php
@@ -8,18 +8,45 @@
  */
 
 return array(
-    'router' => array(
-        'routes' => array(
-            'home' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route'    => '/',
-                    'defaults' => array(
-                        'controller' => 'Album\Controller\Album',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
+	'router' => array(
+		'routes' => array(
+				'home' => array(
+					'type' => 'Zend\Mvc\Router\Http\Literal',
+					'options' => array(
+						'route'    => '/',
+						'defaults' => array(
+							'controller' => 'Album\Controller\Album',
+							'action'     => 'index',
+						),
+					),
+				),
+				
+				/*'teste' => array(
+					'type'    => 'Literal',
+					'options' => array(
+						'route'    => '/application',
+						'defaults' => array(
+							'__NAMESPACE__' => 'Application\Controller',
+							'controller'    => 'Index',
+							'action'        => 'teste',
+						),
+					),
+					'may_terminate' => true,
+					'child_routes' => array(
+						'default' => array(
+							'type'    => 'Segment',
+							'options' => array(
+								'route'    => '/[:controller[/:action]]',
+								'constraints' => array(
+									'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+									'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+								),
+								'defaults' => array(
+								),
+							),
+						),
+					),
+				),	*/
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
@@ -27,10 +54,10 @@ return array(
             'application' => array(
                 'type'    => 'Literal',
                 'options' => array(
-                    'route'    => '/album',
+                    'route'    => '/application',
                     'defaults' => array(
-                        '__NAMESPACE__' => 'Album\Controller',
-                        'controller'    => __NAMESPACE__.'/Controller/'.__NAMESPACE__,
+                        '__NAMESPACE__' => 'Application\Controller',
+                        'controller'    => 'Index',
                         'action'        => 'index',
                     ),
                 ),
@@ -73,7 +100,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Album\Controller\Album' => 'Album\Controller\AlbumController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\Teste' => 'Application\Controller\IndexController'
         ),
     ),
     'view_manager' => array(
@@ -83,7 +111,10 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'album/album/index' => __DIR__ . '/../view/album/album/index.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
