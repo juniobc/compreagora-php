bb4609b745191257c5411ce532c330d1cdb439e9
Sebastião Junio Menezes Campos <juniobc@gmail.com>
2014-09-27 13:51:05 -0300

    Configurado para trabalhar com actions

--- htdocs/zend/module/Application/config/module.config.php
+++ htdocs/zend/module/Application/config/module.config.php
@@ -21,7 +21,7 @@ return array(
 					),
 				),
 				
-				'teste' => array(
+				/*'teste' => array(
 					'type'    => 'Literal',
 					'options' => array(
 						'route'    => '/application',
@@ -46,7 +46,7 @@ return array(
 							),
 						),
 					),
-				),	
+				),	*/
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
